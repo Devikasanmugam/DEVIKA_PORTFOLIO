53dd865160bcd2731f45404fe823ae889e8035da
Devikasanmugam <devikasanmugam06@gmail.com>
2024-09-05 14:07:45 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-let p = 10000;
-let r = 5;
-let t = 3;
-console.log((p*r*t)/100);
-let pi = 3.14159;
-r = 14;
-console.log(pi*r**2);
-let a = 150;
-let b = 200;
-let c = a;
-a = b;
- b = c;
- console.log(a,b);
- let n1 = 85;
- let n2 = 90;
- let n3 = 95;
- console.log()
-
-
-
-
- 
- 
-
-
 
 
 
